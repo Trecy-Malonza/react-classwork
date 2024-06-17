6bea0b5b96fb0f1de34e7e6ecb18ab5d701d36be
Trecy-Malonza <trecymalonza@gmail.com>
2024-06-17 18:48:19 +0300

    added file

---
+++ src/About/index.js
@@ -0,0 +1,11 @@
+import './index.css';
+
+function About (){
+    return(
+        <div>
+        <h2>About Us</h2>
+        <p className ="about-text">We are learning about ReactJS</p>
+        </div>
+    )
+}
+export default About;
